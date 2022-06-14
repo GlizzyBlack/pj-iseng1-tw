352392b576990335b812cb81d97f75042def806b
GlizzyBlack <105611041+GlizzyBlack@users.noreply.github.com>
2022-06-14 18:27:43 +0700

    portofolio tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#38bdf8',
+        secondary: '#64748b',
+        dark: '#18181b',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
